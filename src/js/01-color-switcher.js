4f7154ad57a1b5ebfdc9fdc1d097e8cd29cd47e9
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-07-31 10:14:15 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+let timeId = null;
 
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timeId = setInterval(
+    () => (bodyRef.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+  clearInterval(timeId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
